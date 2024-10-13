84090d707e2aa89c5cd7e3ac2dcfd2d470ab64a1
NagapuriSandeep <96946796+NAGAPURI-SANDEEP@users.noreply.github.com>
2024-10-13 17:25:42 +0530

    Add files via upload

---
+++ First_JavaProgram.java
@@ -0,0 +1,16 @@
+package Day_1;
+
+public class First_JavaProgram 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("WELCOME TO JAVA");
+		System.out.println("WELCOME TO ECLIPSE");
+		System.out.println(10+20);
+		System.out.println(60+20);
+		System.out.println(70+20);
+		
+	}
+	
+
+}
---
+++ Second_JavaProgram.java
@@ -0,0 +1,28 @@
+package Day_1;
+
+public class Second_JavaProgram
+{
+	public static void main(String[] args)
+	{
+		System.out.println("WELCOME TO MAIN METHOD-1");
+		//System.out.println("WELCOME TO MAIN METHOD-2"); single line comments
+		System.out.println("WELCOME TO MAIN METHOD-3");
+		//System.out.println("WELCOME TO MAIN METHOD-4"); single line comments
+		
+		
+		/*System.out.println("ECLIPSE-1");
+		System.out.println("ECLIPSE-2");
+		System.out.println("ECLIPSE-3");
+		System.out.println("ECLIPSE-4"); multiline comments */
+		
+		System.out.println("ECLIPSE-5");
+		
+		System.out.println("ECLIPSE-7");
+		
+		System.out.println(67+89);
+		
+		
+		
+	}
+
+}
