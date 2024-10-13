9725615267ee5fce92429794765f277047b2e7a1
NagapuriSandeep <96946796+NAGAPURI-SANDEEP@users.noreply.github.com>
2024-10-13 17:26:25 +0530

    Add files via upload

---
+++ Data_Types_Demo.java
@@ -0,0 +1,72 @@
+package DAY_2;
+
+public class Data_Types_Demo 
+{
+
+	public static void main(String[] args) 
+	{
+		// Numeric data types
+		int a =100, b=200;
+		System.out.println("The value of a is: "+a);
+		System.out.println("The value of b is: "+b);
+		System.out.println("The sum of a and b is:"+(a+b));
+		
+		//byte 
+		byte d = -18;
+		System.out.println(d);
+		
+		// short
+		short e  =31234;
+		System.out.println(e);
+		
+		//long literal needed
+		long l =2143445566344385445L;
+		System.out.println(l);
+		
+		
+		//decimal numbers  float literal is needed;
+		float flt = 4.8989834f;
+		System.out.println(flt);
+		
+		double dbl = 5.8458945575934759375;
+		System.out.println(dbl);
+		
+		//char  to store single character
+		char grade = 'A';
+		System.out.println(grade);
+		
+		// String to store multiple characters, In double quotes
+		String name = "JHON";
+		System.out.println(name);
+		
+		//   char ch="f";
+		//   String nm='jhon';
+		//   String nm='A'
+		
+		String ch="J";
+		
+		// boolean is used to represent the status 
+		boolean Is_working = true;
+		boolean is_present = false;
+		
+		System.out.println(Is_working);
+		System.out.println(is_present);
+		
+		// boolean bl ="true";
+		// boolean bl ="false";
+		// String bl =true;
+		// String bl =false;
+		
+		String bl ="true";
+		System.out.println(bl);
+		
+		
+		
+		
+		
+				
+		
+
+	}
+
+}
---
+++ Variables_Demo.java
@@ -0,0 +1,47 @@
+package DAY_2;
+
+public class Variables_Demo 
+{
+
+	public static void main(String[] args)
+	{
+		int x;   // Declaration 
+		x=100;  //  Assign value 
+		System.out.println(x);
+
+		int y=100; //datatype, variable, value,  declaration and assignment
+		System.out.println(y);
+		
+		y=200;
+		System.out.println(y);
+		int z=200;
+		System.out.println(z);
+		
+		// Approach-1
+		int s = 150;
+		int t = 250;
+		int u = 350;
+		
+		// Approach-2 same data type
+		
+		int  a,b,c;
+		a=500;
+		b=600;
+		c=700;
+		
+		//Approach-3  same data type
+		int g=100, h=500, i=700;
+		System.out.println(" :The value of g is :"+g);
+		System.out.println(" :The value of h is :"+h);
+		System.out.println(" :The value of i is :"+i);
+		
+		//Approach-4
+		System.out.println(g+" "+h+" "+i);
+		
+		
+		
+		
+		
+	}
+
+}
